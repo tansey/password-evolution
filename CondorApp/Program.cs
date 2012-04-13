47542fda85e82f92bd00b20cd98c165c195bc056
Wesley <wes.tansey@gmail.com>
2012-04-13 17:25:24 -0500

    Added condor app
    Former-commit-id: d6a8532ed5ceda8c5c9e6c6128a63f3893b055a1

---
+++ CondorApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CondorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int curArg = 0;
+            string experimentDir = args[curArg++];
+            string configFile = args[curArg++];
+        }
+    }
+}
