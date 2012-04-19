241641fd6c9b6219d907c63e562fb8777eb88c84
Edaena Salinas <Edaena@Edaena-PC.(none)>
2012-04-19 16:23:57 -0500

    Added some help options and reading of some parameters

--- CondorApp/CondorParameters.cs
+++ CondorApp/CondorParameters.cs
@@ -8,6 +8,8 @@ namespace CondorApp
     class CondorParameters
     {
         public string Name { get; set; }
+        public string ExperimentDir { get; set; }
+        public string ConfigFile { get; set; }
         public int EnsembleSize {get; set;}
         public int Generations { get; set; }
         public int EnsembleGuesses {get; set;} //allocation -> guess weighting
@@ -26,6 +28,36 @@ namespace CondorApp
         {
             CondorParameters cp = DefaultParameters(args[0]);
 
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i][0] != '-')
+                {
+                    Console.WriteLine("Invalid option: '{0}'. Options must be prefixed with '-'", args[i]); return null;
+                }
+
+                switch (args[i].Substring(1).Trim().ToLower())
+                {
+                    case "help": PrintHelp(); return null;
+                    case "name":
+                    case "n":
+                        string name = args[++i];
+                        cp.Name = name;
+                        break;
+                    case "experimentdir":
+                    case "expdir":
+                        string experimentDir = args[++i];
+                        cp.ExperimentDir = experimentDir;
+                        break;
+                    case "config":
+                    case "c":
+                        string configFile = args[++i];
+                        cp.ConfigFile = configFile;
+                        break;
+
+                }
+
+            }
+            
             return cp;
         }
 
@@ -50,5 +82,28 @@ namespace CondorApp
 
             return cp;
         }
+
+       static void PrintHelp()
+        {
+            Console.WriteLine("Usage: AgentBenchmark.exe <name> [-options...]");
+            Console.WriteLine("<name>".PadRight(25) + "File prefix to use when saving the config and results files.");
+            Console.WriteLine("-help".PadRight(25) + "Prints the usage summary.");
+            Console.WriteLine("-name -n".PadRight(25) + "Name of the experiment.");
+            Console.WriteLine("-experimentdir -expdir".PadRight(25) + "Directory of the experiment.");
+            Console.WriteLine("-config -c".PadRight(25) + "Name of the config file");
+
+            Console.WriteLine("-game -g".PadRight(25) + "Name of the game. Valid options: tictactoe, connect4, reversi. Default: tictactoe");
+            Console.WriteLine("-inputs -i".PadRight(25) + "Number of inputs for the neural network (usually # of board spaces). Default: 9");
+            Console.WriteLine("-outputs -o".PadRight(25) + "Number of outputs for the neural network (usually # of board spaces). Default: 9");
+            Console.WriteLine("-evaluator -eval -e".PadRight(25) + "Evaluation function to use. Valid options: random, coevolve, minimax, blondie, mcts. Default: random");
+            Console.WriteLine("-name -n".PadRight(25) + "Name of the experiment.");
+            Console.WriteLine("-winreward -win".PadRight(25) + "Reward an agent receives for winning a game. Default: 2");
+            Console.WriteLine("-tiereward -tie".PadRight(25) + "Reward an agent receives for tying a game. Default: 1");
+
+        }
+
+
+
+
     }
 }
